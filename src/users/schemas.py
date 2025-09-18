1e0ffeeeba8df97ee78fecb038016b7a95cd17ad
Woohyuk Jang <morrranii@gmail.com>
2025-09-18 13:45:36 +0900

    Fix Imports

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -3,7 +3,8 @@ import re
 from pydantic import BaseModel, field_validator, EmailStr
 from fastapi import HTTPException
 
-from users.errors import InvalidPasswordException
+from src.users.errors import InvalidPasswordException
+
 
 class CreateUserRequest(BaseModel):
     name: str
@@ -13,19 +14,20 @@ class CreateUserRequest(BaseModel):
     bio: str | None = None
     height: float
 
-    @field_validator('password', mode='after')
+    @field_validator("password", mode="after")
     def validate_password(cls, v):
         if len(v) < 8 or len(v) > 20:
             raise InvalidPasswordException()
         return v
-    
-    @field_validator('phone_number', mode='after')
+
+    @field_validator("phone_number", mode="after")
     def validate_phone_number(cls, v):
         pass
 
-    @field_validator('bio', mode='after')
+    @field_validator("bio", mode="after")
     def validate_bio(cls, v):
         pass
+
 
 class UserResponse(BaseModel):
     user_id: int
@@ -34,3 +36,4 @@ class UserResponse(BaseModel):
     phone_number: str
     bio: str | None = None
     height: float
+
