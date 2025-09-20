544d69f07f12b11a9dd0fda07c0311feb32be606
Woohyuk Jang <morrranii@gmail.com>
2025-09-20 19:40:04 +0900

    Add POST /auth/token endpoint

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,14 +1,66 @@
+import datetime
+import argon2
 from fastapi import APIRouter
 from fastapi import Depends, Cookie
 
+from src.auth.schemas import LoginRequest, LoginResponse
 from src.common.database import blocked_token_db, session_db, user_db
+import jwt
+from src.users.errors import InvalidAccountException
+from datetime import datetime, timedelta, timezone
+from src.auth.password_hash import password_hasher
+import secrets
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 SHORT_SESSION_LIFESPAN = 15
 LONG_SESSION_LIFESPAN = 24 * 60
 
-# @auth_router.post("/token")
+SECRET_KEY = secrets.token_bytes(
+    32
+)  # use an in-memory secret key. we use an in-memory db anyway
+
+
+@auth_router.post("/token")
+def login(request: LoginRequest) -> LoginResponse:
+    try:
+        for user_dict in user_db:
+            if user_dict["email"] == request.email:
+                password_hasher.verify(user_dict["hashed_password"], request.password)
+                # create access token
+                access_token_claims = {
+                    "sub": user_dict["email"],
+                    "exp": int(
+                        (
+                            datetime.now(tz=timezone.utc)
+                            + timedelta(minutes=SHORT_SESSION_LIFESPAN)
+                        ).timestamp()
+                    ),
+                }
+                access_token = jwt.encode(
+                    access_token_claims, SECRET_KEY, algorithm="HS256"
+                )
+                # create refresh token
+                refresh_token_claims = {
+                    "sub": user_dict["email"],
+                    "exp": int(
+                        (
+                            datetime.now(tz=timezone.utc)
+                            + timedelta(minutes=LONG_SESSION_LIFESPAN)
+                        ).timestamp()
+                    ),
+                }
+                refresh_token = jwt.encode(
+                    refresh_token_claims, SECRET_KEY, algorithm="HS256"
+                )
+                return LoginResponse(
+                    access_token=access_token, refresh_token=refresh_token
+                )
+    except argon2.exceptions.VerifyMismatchError:
+        raise InvalidAccountException()
+    raise InvalidAccountException()
+
+
 #
 #
 # @auth_router.post("/token/refresh")
--- src/users/router.py
+++ src/users/router.py
@@ -3,11 +3,12 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, Cookie, Header, status
 
 from src.users.schemas import CreateUserRequest, UserResponse
-from argon2 import PasswordHasher
 from src.common.database import blocked_token_db, session_db, user_db
+from argon2 import PasswordHasher
+from src.auth.password_hash import password_hasher
+
 
 user_router = APIRouter(prefix="/users", tags=["users"])
-password_hasher = PasswordHasher()
 
 
 @user_router.post("/", status_code=status.HTTP_201_CREATED)
@@ -16,7 +17,7 @@ def create_user(request: CreateUserRequest) -> UserResponse:
     user_dict["hashed_password"] = password_hasher.hash(user_dict["password"])
     del user_dict["password"]
     user_db.append(user_dict)
-    return UserResponse(user_id=len(user_db)-1, **user_dict)
+    return UserResponse(user_id=len(user_db) - 1, **user_dict)
 
 
 @user_router.get("/me")
