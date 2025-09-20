d884e44e492015d0fec13c308184035da1c6263d
Woohyuk Jang <morrranii@gmail.com>
2025-09-20 21:26:20 +0900

    Move validation logic inside GET

--- src/users/router.py
+++ src/users/router.py
@@ -1,4 +1,14 @@
 from typing import Annotated
+import jwt
+from src.auth.router import SECRET_KEY
+from src.users.errors import (
+    BadAuthHeaderException,
+    UnauthenticatedException,
+    InvalidTokenException,
+)
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi import Depends
+from typing import Optional
 
 from fastapi import APIRouter, Depends, Cookie, Header, status
 
@@ -21,9 +31,31 @@ def create_user(request: CreateUserRequest) -> UserResponse:
     return UserResponse(user_id=len(user_db) - 1, **user_dict)
 
 
+bearer_scheme = HTTPBearer(auto_error=False)
+
+
 @user_router.get("/me")
 def get_user_info(
-    user_id: int = Depends(validate_token_and_extract_user_id),
+    credential: Optional[HTTPAuthorizationCredentials] = Depends(bearer_scheme),
 ) -> UserResponse:
+    if not credential:
+        raise UnauthenticatedException()
+    if credential.scheme != "Bearer":
+        raise BadAuthHeaderException()
+
+    token = credential.credentials
+    try:
+        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+    except jwt.PyJWTError as e:
+        print(type(e))
+        print(e)
+        raise InvalidTokenException()
+
+    try:
+        user_id = decoded_token["sub"]
+        user_id = int(user_id[-1])
+    except Exception:
+        raise InvalidTokenException()
+
     user_dict = user_db[user_id]
     return UserResponse(user_id=user_id, **user_dict)
